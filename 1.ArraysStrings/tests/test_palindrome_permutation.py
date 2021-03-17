939a2567ca372340fd3e237299a3b18fa8de06da
e2166460 <e2166460@ceng.metu.edu.tr>
2021-03-17 13:47:23 +0300

    tests addded

---
+++ 1.ArraysStrings/tests/test_palindrome_permutation.py
@@ -0,0 +1,34 @@
+import unittest
+import palindromePermutation
+
+class isUniqueTest(unittest.TestCase):
+    
+    def test_empty(self):
+        self.assertTrue(palindromePermutation.hashTableSolution(""))
+        
+    def test_all_space(self):
+        self.assertTrue(palindromePermutation.hashTableSolution("     "))
+
+    def test_true_input(self):
+        self.assertTrue(palindromePermutation.hashTableSolution("aaavv"))
+    
+    def test_false_input(self):
+        self.assertFalse(palindromePermutation.hashTableSolution("aaavvv"))
+    
+    def test_false_input_with_space(self):
+        self.assertFalse(palindromePermutation.hashTableSolution("aaa    vvv"))
+    
+    def test_true_input_with_space(self):
+        self.assertTrue(palindromePermutation.hashTableSolution("asd    asd"))
+    
+    def test_true_non_letters(self):
+        self.assertTrue(palindromePermutation.hashTableSolution(".,:;;;;<>"))
+
+    def test_false_input_with_nonletter(self):
+        self.assertFalse(palindromePermutation.hashTableSolution("klme...,,"))
+
+    def test_true_input_with_numeric(self):
+        self.assertTrue(palindromePermutation.hashTableSolution("21654978"))
+
+    def test_false_input_with_numeric(self):
+        self.assertFalse(palindromePermutation.hashTableSolution("21654asd978"))
