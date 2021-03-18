28829e25bcc5633bce30d4c76d0769e48279f6a4
e2166460 <e2166460@ceng.metu.edu.tr>
2021-03-18 11:03:11 +0300

    tests added

---
+++ 1.ArraysStrings/tests/test_string_compression.py
@@ -0,0 +1,24 @@
+import stringCompression
+import unittest
+
+class palindromePermutationTest(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(stringCompression.onePassSolution(""),"")
+
+    def test_same_letters(self):
+        self.assertEqual(stringCompression.onePassSolution("aaaa"),"a4")
+
+    def test_no_compression(self):
+        self.assertEqual(stringCompression.onePassSolution("abcavse"),"abcavse")
+
+    def test_valid_input(self):
+        self.assertEqual(stringCompression.onePassSolution("aaabbccca"),"a3b2c3a1")
+        self.assertEqual(stringCompression.onePassSolution("aabcccccaaa"),"a2b1c5a3")
+
+    
+
+
+        
+
+
